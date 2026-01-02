c9b9ffee17c8f116d20e60efefe52879e9c2067a
Vitor Palacio <vitorpalacio@gmail.com>
2026-01-02 19:36:11 -0300

    Configuração para deploy no Render

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Olá, Flask! 🚀"
+    return "Flask rodando no Render 🚀"
 
-@app.route("/sobre")
-def sobre():
-    return "Página sobre o projeto"
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
