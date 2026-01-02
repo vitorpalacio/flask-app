0aa2b846c882b08f481cfdda8e641999a8686886
Vitor Palacio <vitorpalacio@gmail.com>
2026-01-02 19:45:09 -0300

    Atualiza rota X

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask rodando no Render 🚀"
+    return "Flask rodando no Render 🚀🚀🚀"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
